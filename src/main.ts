6aed87fe980ac12fca43986cbcde22aaa5e53576
tabib-e-alahi <tabib.e.alahi@gmail.com>
2026-09-20 19:49:03 +0600

    Update main.ts, Update tsconfig.build.tsbuildinfo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle("User Mangement Syste,").setDescription("This system contains api configuration of user profile management")
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
