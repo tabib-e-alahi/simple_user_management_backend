136c2edba93579aed512493d866b56d848714403
tabib-e-alahi <tabib.e.alahi@gmail.com>
2026-09-21 09:44:56 +0600

    Refactor code structure for improved readability and maintainability

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
+
+    @Post('register')
+    register(){
+        return "hellow from register"
+    }
 
     @Get()
     findOne() {
